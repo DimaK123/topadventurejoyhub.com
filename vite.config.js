7d85aa8b1ff554971ddf482e9aa3d1f93230a098
Dima <dimak119988@gmail.com>
2025-01-14 00:41:19 +0200

    Beta 0.3

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/topadventurejoyhub.com/',
+  // base: '/topadventurejoyhub.com/',
   build: {
     outDir: 'docs', // Папка для сборки
   },
   plugins: [react()],
-  // base: './',
+  base: './',
 })
